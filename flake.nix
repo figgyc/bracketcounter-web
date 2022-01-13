2a59d4c61edcc0c94411595e902daf695fe756a5
George Brooke <figgyc@figgyc.uk>
2022-01-13 00:49:08 +0000

    update nix build

--- flake.nix
+++ flake.nix
@@ -3,18 +3,16 @@
 
   inputs.flake-utils.url = "github:numtide/flake-utils";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs";
-  inputs.node2nix.url = "github:svanderburg/node2nix";
-  inputs.node2nix.flake = false;
 
-  outputs = { self, nixpkgs, flake-utils, node2nix }:
+  outputs = { self, nixpkgs, flake-utils }:
     {
       # Nixpkgs overlay providing the application
       overlay =
         (final: prev: {
           # The application
-          bracketcounter-web = prev.pkgs.callPackage ./. {
-            src = self;
-            node2nix = (prev.pkgs.callPackage node2nix {}).package;
+          bracketcounter-web = prev.pkgs.mkYarnPackage {
+            src = ./.;
+            name = "bracketcounter-web";
           };
         });
     } // (flake-utils.lib.eachDefaultSystem (system:
